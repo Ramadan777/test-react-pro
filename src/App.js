897a3d9347327e19794ac68a8256f65701c50c0e
Ramadan777 <faysol@mail.ru>
2020-12-12 14:04:49 +0300

    add test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  // 1. Замена всех отрицательных чисел
+  // Есть массив из чисел `[2, -2, 0.1, 0, 0, -10, -9.9, -0.5, 0.3, 0.003, 2]`.
+  // Напиши код, который все отрицательные значения элементов массива заменит на нолик.
+  //
+  // В итоге должно получиться: `[2, 0, 0.1, 0, 0, 0, 0, 0, 0.3, 0.003, 2]`.
+
+  const numbers = [2, -2, 0.1, 0, 0, -10, -9.9, -0.5, 0.3, 0.003, 2];
+
+  const newNumbers = numbers.map(number => {
+    if(number < 0) {
+      return 0;
+    }
+    return  number;
+  })
+
+  console.log(newNumbers)
+
+
+  // 2. Избавляемся от близнецов
+  // Дан массив из объектов:
+  //Обработай массив так, чтобы в итоге остались только те элементы,
+  // у которых значения свойств a и b не совпадают.
+
+  const elements = [
+    { a: 2, b: 12 },
+    { a: 0.1, b: 0.1 },
+    { a: 0, b: 1 },
+    { a: 100, b: 1000 },
+    { a: 4, b: 4 }
+    ];
+
+   const result = elements.filter(item => item.a !== item.b)
+    console.log(result);
+
+   //  3. Поиск близнеца
+  // Напиши код, который в массиве из предыдущего задания найдет
+  // какого-нибудь близнеца (объекта, у которого значения обеих свойств равны)
+  // и вернет сумму этих значений.
+
+
+  const arr = elements.find(item => item.a === item.b)
+  const newArr = arr.a + arr.b;
+
+  console.log(newArr);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     1
+   </div>
   );
 }
 
